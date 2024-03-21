b5c1a58c5b62e046e4c9dab95cf33bf0890957dd
EMugisha2 <e.mugisha2@alustudent.com>
2024-03-20 23:14:53 -0700

    conctatenating edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = (str[39:66] + str[106:112] + str[:6])
-print(str)
+s = "Python is an interpreted, interactive, object-oriented programming \
+language that combines remarkable power with very clear syntax"
+s = (s[39:66] + s[106:112] + s[:6])
+print(s)
+
