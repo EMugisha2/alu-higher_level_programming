2d1f07618c9823f1dbbfb6e8508315be563e78be
EMugisha2 <e.mugisha2@alustudent.com>
2024-04-13 10:18:41 -0700

    x

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    print(sum(map(int, sys.argv[1:])))
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
