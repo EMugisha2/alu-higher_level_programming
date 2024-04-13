1f56cfb9345aded1c4d6b76aa3a6a1f530348d0e
EMugisha2 <e.mugisha2@alustudent.com>
2024-04-13 10:14:01 -0700

    x

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("")
+def print_last_digit(number):
+    print(abs(number) % 10, end='')
+    return(abs(number) % 10)
